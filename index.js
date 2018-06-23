38b5c4742d29cec7a5c7a35acc749ac223cb2ae1
nmoun <39025757+nmoun@users.noreply.github.com>
2018-06-23 21:58:42 +0200

    legend

--- index.js
+++ index.js
@@ -31,10 +31,15 @@ fetch('file:///C:/Users/Nicolas/Documents/projects/d3-idf/salaries.json')
 
 });
 
-
+var max = 1, min = -11, rangeLegend = [600, 1000];
 var xLegend = d3.scaleLinear()
-.domain([1, 10])
-.rangeRound([600, 860]);
+    .domain([min, max])
+    .rangeRound(rangeLegend);
+
+var xAxis = d3.axisBottom(xLegend)
+    .tickSize(10)
+    .tickValues(d3.range(min + (parseFloat(max)- parseFloat(min))/5, max, (parseFloat(max)- parseFloat(min))/5).map(d => d.toFixed(2)))
+    .tickFormat(function(n) { return n + "€/h"});
 
 var g = svg.append("g")
 .attr("class", "key")
@@ -48,6 +53,64 @@ g.append("text")
   .attr("text-anchor", "start")
   .attr("font-weight", "bold")
   .text("Difference salaire 2014");
+
+createLinearGradient(min, max);
+
+g.append("rect")
+    .attr("x", xLegend.range()[0])
+    .attr("y", 0)
+    .attr("width", xLegend.range()[1]-xLegend.range()[0])
+    .attr("height", 8)
+    .style("fill", "url(#linear-gradient)"); 
+
+g.append("g").call(xAxis);
+
+function getAxis(min, max){
+  var scale = d3.scaleLinear()
+    .domain([min, max])
+    .rangeRound(rangeLegend);
+
+  var xAxis = d3.axisBottom(scale)
+    .tickSize(10)
+    .tickValues(d3.range(min + (parseFloat(max)- parseFloat(min))/5, max, (parseFloat(max)- parseFloat(min))/5).map(d => d.toFixed(2)))
+    .tickFormat(function(n) { return n + "€/h"});
+
+  return xAxis;
+}
+
+function createLinearGradient(min, max){
+
+  svg.selectAll("defs").remove();
+  
+  var positionZero = d3.scaleLinear()
+    .domain([min, max])
+    .range([0, 100]);
+
+  console.log('percent zero ' + positionZero(0))
+    
+  var linearGradient = svg.append("defs")
+    .append("linearGradient")
+    .attr("id", "linear-gradient");
+
+  if(min < 0){
+    linearGradient.append("stop")
+      .attr("offset", "0%")
+      .attr("stop-color", '#cc0000');
+
+    var offset = positionZero(0)
+    linearGradient.append("stop")
+      .attr("offset", offset + "%")
+      .attr("stop-color", '#ffffff');
+  } else {
+    linearGradient.append("stop")
+      .attr("offset", "0%")
+      .attr("stop-color", '#ffffff');
+  }
+
+  linearGradient.append("stop")
+    .attr("offset", "100%")
+    .attr("stop-color", '#0066ff');
+};
 
 function changeAge(){
   var select = document.getElementById("age-select");
@@ -71,9 +134,9 @@ function changeAge(){
 
   console.log(JSON.stringify(citiesFeaturesCollection))
 
-  console.log("b: " + b)	
-  console.log("scale : " + s)	
-  console.log("translate : " + t)	
+  // console.log("b: " + b)	
+  // console.log("scale : " + s)	
+  // console.log("translate : " + t)	
 
   // Update the projection to use computed scale & translate.
   projection
@@ -84,18 +147,38 @@ function changeAge(){
   var tmp = citiesFeaturesCollection.features.map(function(v){
     return v.properties[h] - v.properties[f];
   })
-  var min = Math.min.apply(null, tmp);
-  var max = Math.max.apply(null, tmp);
+  min = Math.min.apply(null, tmp);
+  max = Math.max.apply(null, tmp);
   console.log("différence minimum = " + min)
   console.log("différence max = " + max)
-  var color = d3.scaleLinear()
-    .domain([min, 0 ,max])
+  var color = d3.scaleLinear();
+
+  if(min < 0){
+    color.domain([min, 0 ,max])
     .range(["red", "white", "blue"]);
+  } else {
+    color.domain([min, max])
+    .range(["white", "blue"]);
+  }
 
   console.log("exemple color min: " + color(min))
   console.log("exemple color max: " + color(max))
 
-  var paths = svg.selectAll("path")
+  // LA LEGENDE
+  createLinearGradient(min, max);
+  // xLegend.domain([min, max])
+  axe = getAxis(min, max);
+  g.selectAll("g").remove();//.attr('transform',"translate(0," + (parseFloat(max)- parseFloat(min))/5 + ")").call(xAxis);
+  g.append("g").call(axe);
+  // legend.call(xAxis);
+
+  // LA MAP
+  var paths = svg
+      .append('g')
+      .selectAll("path")
+      .filter(function(d){
+        return d.attr('class').indexOf('domain') !== 'domain'
+      })
       .data(citiesFeaturesCollection.features, function(d) { return d.codgeo; })
       .attr("fill",function(d){
         return color(Math.abs(d.properties[h] - d.properties[f]))
